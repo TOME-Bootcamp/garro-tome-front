9c59bd0a9540e12cb0500c209ac0645f8814ff34
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-08-14 21:27:40 -0300

    initial

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.gr-assets.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
